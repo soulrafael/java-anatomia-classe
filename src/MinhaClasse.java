0016dc00c518b40494522c8e1ae185fec39fc08b
soulrafael <rafaelbsoul@gmail.com>
2024-05-17 00:14:25 -0300

    novo comite

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,7 +1,14 @@
 public class MinhaClasse {
 
     public static void main(String[] args){
+        String BR="Brasil";
+        int PI= 3.14;
+
+
+
         System.out.println("Hello World");
+        System.out.println(PI);
+        System.out.println(BR);
     }
     
 }
