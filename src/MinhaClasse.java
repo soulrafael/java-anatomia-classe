5b68998bbef92c2c30e9952b0a064c783928c65e
soulrafael <rafaelbsoul@gmail.com>
2024-05-17 08:34:22 -0300

    evoluindo

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,13 +2,28 @@ public class MinhaClasse {
 
     public static void main(String[] args){
         String BR="Brasil";
-        int PI= 3.14;
-
-
+        double PI= 3.14;
+        String x="A";
+        String y="B";
 
         System.out.println("Hello World");
         System.out.println(PI);
         System.out.println(BR);
+        NomeCompleto(x,y);
     }
-    
+    /**
+     * @param primeiroNome
+     * @param segundoNome
+     */
+    static public String NomeCompleto(String primeiroNome, String segundoNome){
+primeiroNome="Rafael";
+segundoNome="Larissa";
+        System.out.println(primeiroNome+segundoNome);
+        return primeiroNome+segundoNome;
+    }
+
+
+
+
+
 }
